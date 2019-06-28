3f18bdbbe983b83b385bc452b8035254d0530531
Axel Catelan <axel.catelan@gmail.com>
2019-06-28 18:20:45 +0200

    Tutorial 5 : Relationships & Hyperlinked API
    - Add hyperlink on the browsable api so user can navigate easily
    - Add Pagination

--- djangorest/api/serializers.py
+++ djangorest/api/serializers.py
@@ -2,21 +2,22 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from api.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username') # Same as using a CharField(read_only=True)
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ('id', 'owner', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('url', 'highlight', 'id', 'owner', 'title', 'code', 'linenos', 'language', 'style')
         
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     # We need to add snippets manually because it's created by the ForeignKey in the Snippet class, and not directly in the User model
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
+        fields = ('url', 'id', 'username', 'snippets')
 
 
 # # Below is how we would implement the serializer without using ModelSerializer
