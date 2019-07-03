34a539195c2df5a1de56f3caa02d5a758558a48c
Axel Catelan <axel.catelan@gmail.com>
2019-07-03 14:26:21 +0200

    Tutorial 7 : Schemas & client libraries
    Add schema so the API is usable with client libraries (tested with 'coreapi')

--- djangorest/api/urls.py
+++ djangorest/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 from api import views
 
 # Create a router and register our viewset with it
@@ -7,7 +8,10 @@ router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
+schema_view = get_schema_view(title='Snippet API')
+
 # The API urls re now determined automatically by the router
 urlpatterns = [
+    path('schema/', schema_view),
     path('', include(router.urls)),
 ]
