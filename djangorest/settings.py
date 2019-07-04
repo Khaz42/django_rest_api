803f3247229233eb51bd2a6d38b801faf5933ffd
Axel Catelan <axel.catelan@gmail.com>
2019-07-04 18:47:04 +0200

    last commit

--- djangorest/settings.py
+++ djangorest/settings.py
@@ -21,14 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY', 'localkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['djangorestapp.herokuapp.com']
-ALLOWED_HOSTS = ['localhost']
-
+ALLOWED_HOSTS = ['djangorestapp.herokuapp.com', 'localhost']
 
 # Application definition
 
